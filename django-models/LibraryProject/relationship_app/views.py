a1b9e14a796d51a16b14c5a95482ecfec24252c7
AdolfMkonda <adolfanatory@gmail.com>
2025-09-08 15:37:42 +0300

    add: implement list_books view to return all books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Books, Librarian, Library
 
 # Create your views here.
+from django.http import HttpResponse
+
+def list_books(request):
+    return HttpResponse(Books.objects.all())
+
+class library_DetailView():
+    model = Library
+    context_object_name = 'library'
