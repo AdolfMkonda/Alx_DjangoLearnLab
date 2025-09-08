34092adb13128730c13847d355b46370264e95cf
AdolfMkonda <adolfanatory@gmail.com>
2025-09-08 15:43:13 +0300

    fix: update list_books view to render template with book data

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,8 +5,10 @@ from .models import Books, Librarian, Library
 from django.http import HttpResponse
 
 def list_books(request):
-    return HttpResponse(Books.objects.all())
+    books = Books.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
 class library_DetailView():
     model = Library
+    template_name = 'library_detail.html'
     context_object_name = 'library'
