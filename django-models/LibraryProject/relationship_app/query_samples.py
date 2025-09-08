90cc3ca7b40c7e5870073c7b5bc21974c4e5f861
AdolfMkonda <adolfanatory@gmail.com>
2025-09-08 15:18:36 +0300

    refactor query_samples.py to correct author retrieval and filter syntax

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,12 +2,10 @@ from relationship_app.models import Author, Book, Librarian, Library
 
 Book.objects.get(Book.all())
 
-Author.objects.get(id = 1)
-
 Library.objects.get(name=library_name)
 Library.books.all()
 
 Librarian.objects.get(id = 1)
 
-Author.objects.get(name = author_name)
-object.filter(Author=Author)
+Author.objects.get(name=author_name)
+object.filter(author=author)
