6796cb58967f3ea67c7336cc079fae9582458b84
AdolfMkonda <adolfanatory@gmail.com>
2025-09-08 15:21:58 +0300

    fix: correct placement of author filter in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import Author, Book, Librarian, Library
 
 Book.objects.get(Book.all())
-object.filter(author=author)
+
 
 Library.objects.get(name=library_name)
 Library.books.all()
@@ -9,4 +9,7 @@ Library.books.all()
 Librarian.objects.get(id = 1)
 
 Author.objects.get(name=author_name)
+object.filter(author=author)
+
+
 object.filter(author=author)
