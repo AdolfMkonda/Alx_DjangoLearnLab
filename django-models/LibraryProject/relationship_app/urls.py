fbf7fe38520ecdaf5818edb81579f49cfe569dfd
AdolfMkonda <adolfanatory@gmail.com>
2025-09-09 15:13:45 +0300

    feat: update authentication URL patterns for login, logout, and registration views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ app_name = 'relationship_app'
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),    
+    path('login/', loginView.as_view(template_name="")),
+    path('logout/', views.logoutView, name='logout'),
+    path('register/', views.registerView, name='register'),    
 ]
