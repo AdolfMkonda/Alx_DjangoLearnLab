153e9163092d16336e774225b05b3dc556eace98
AdolfMkonda <adolfanatory@gmail.com>
2025-09-09 15:06:49 +0300

    fix: reorder import statements for clarity in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
