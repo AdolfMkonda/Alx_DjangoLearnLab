4f548ee676f57978dad2eff13bb2c862404d2c04
AdolfMkonda <adolfanatory@gmail.com>
2025-09-09 15:46:56 +0300

    fix: set template names for login and logout views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ app_name = 'relationship_app'
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', loginView.as_view(template_name="")),
-    path('logout/', logoutView.as_view(template_name="")),
+    path('login/', loginView.as_view(template_name="login.html")),
+    path('logout/', logoutView.as_view(template_name="logout.html")),
     path('register/', views.registerView, name='register'),    
 ]
