cbbafb04d09acf995e63302ec1493d189dae6af6
AdolfMkonda <adolfanatory@gmail.com>
2025-09-11 19:55:41 +0300

    Add permission checks for book management views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.decorators import permission_required
 
 
 
@@ -24,6 +25,7 @@ can_view_permission = Permission.objects.get(codename='can_view')
 group.permissions.add(can_view_permission)
 print("Permission 'can_view' added to group 'Viewers'.")
 
+
 group, created = Group.objects.get_or_create(name='Admins')
 if created:
     print("Group 'Admins' created.")
@@ -33,11 +35,30 @@ can_delete_permission = Permission.objects.get(codename='can_delete')
 group.permissions.add(can_delete_permission)
 print("Permission 'can_delete' added to group 'Admins'.")
 can_edit_permission = Permission.objects.get(codename='can_edit')
+
 group.permissions.add(can_edit_permission)
 print("Permission 'can_edit' added to group 'Admins'.")
 can_view_permission = Permission.objects.get(codename='can_view')
 group.permissions.add(can_view_permission)
 print("Permission 'can_view' added to group 'Admins'.")
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    return render(request, 'bookshelf/view_books.html') 
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_books(request):
+    return render(request, 'bookshelf/edit_books.html') 
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_books(request):
+    return render(request, 'bookshelf/delete_books.html')
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_books(request):
+    return render(request, 'bookshelf/create_books.html')
+
+
 
 
     
