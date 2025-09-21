e2fb447e18d3439473f5e6a2966682c3a9a3d645
AdolfMkonda <adolfanatory@gmail.com>
2025-09-21 22:32:35 +0300

    bug fix

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from rest_framework.permissions import IsAuthenticated
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.permissions.IsAuthenticated',
 ]
 
 # Application definition
