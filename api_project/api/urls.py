7edc71bd493dfb027dbaaad4d62a66782eba7b88
AdolfMkonda <adolfanatory@gmail.com>
2025-09-21 22:00:33 +0300

    implementing CRUD OP

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.db import router
+from django.urls import include, path
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+router = DefaultRouter()
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),  # Include the router URLs
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,18 @@
+from django.db import router
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 
+
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+router.register(r'books_all', BookViewSet, basename='book_all')
