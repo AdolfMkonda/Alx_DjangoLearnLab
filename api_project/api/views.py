f1adacd8be5496c7de9df27927ac75a8da68adcb
AdolfMkonda <adolfanatory@gmail.com>
2025-09-21 21:40:36 +0300

    fix api view list

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from api_project.api.serializers import BookSerializer
 from .models import Book
-
+from rest_framework import generics
 
 # Create your views here.
-class BookList(viewsets.ModelViewSet):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
