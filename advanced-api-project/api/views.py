6ab385f0253e40ce4c9865f3e7c1587f3924ffd8
AdolfMkonda <adolfanatory@gmail.com>
2025-09-26 01:21:37 +0300

    fixing library importation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Book
-from .rest_framework import viewsets
+from rest_framework import viewsets
 from .serializers import BookSerializer
+from rest_framework.permissions import isAuthenticated, IsAuthenticatedOrReadOnly
+
 
 
 # Create your views here.
