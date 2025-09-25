ad6cc88d723f04652034a0597b81b1999631387f
AdolfMkonda <adolfanatory@gmail.com>
2025-09-26 01:23:30 +0300

    importation fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import viewsets
 from .serializers import BookSerializer
-from rest_framework.permissions import isAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, isAuthenticated
 
 
 
