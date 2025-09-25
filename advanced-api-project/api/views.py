4d2349ee261c270d8eac9451225f40b11746f758
AdolfMkonda <adolfanatory@gmail.com>
2025-09-26 01:04:41 +0300

    using proper view name

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from .serializers import BookSerializer
 # Create your views here.
 
 
-class listView(viewsets.ModelViewSet):
+class ListView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
