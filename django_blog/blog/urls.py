5c767b508fe808b0abc3fe5f2ed19c5ef0475060
AdolfMkonda <adolfanatory@gmail.com>
2025-10-03 21:24:43 +0300

    Implementing user Authentication feature

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.urls import include
+from .views import SignUpView, HomeView, logged_in_view
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from . import views
+
+urlpatterns = [
+    path('', HomeView.home, name='home'),
+    path('signup/', SignUpView.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logged_in/', login_required(views.logged_in_view), name='logged_in'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+class SignUpView:
+    def signup(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect('home')
+        else:
+            form = UserCreationForm()
+        return render(request, 'registration/signup.html', {'form': form})
+    
+class HomeView:
+    def home(request):
+        return render(request, 'home.html')
+
+class logged_in_view:
+    @login_required
+    def logged_in_view(request):
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return render(request, 'logged_in.html', {'posts': posts})
+
