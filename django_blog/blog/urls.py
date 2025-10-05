3db928742db4027c2ece90dc92078f2e583d71f0
AdolfMkonda <adolfanatory@gmail.com>
2025-10-05 16:36:36 +0300

    Adding correct urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('posts/new/', views.post_new, name='post_new'),
-    path('posts/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/new/', views.post_create, name='post_create'),
 ]
