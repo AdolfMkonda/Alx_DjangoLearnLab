97cc741d1821e24d7c09127cb88d8f45e74d0284
AdolfMkonda <adolfanatory@gmail.com>
2025-10-10 21:10:17 +0300

    bug fix

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,10 +5,6 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 
 
-class modelViewSet(viewsets.ModelViewSet):
-    queryset = user.objects.all()
-    serializer_class = UserSerializer   
-
 class loginViewSet(viewsets.ModelViewSet):
     queryset = user.objects.all()
     serializer_class = UserSerializer
@@ -16,11 +12,15 @@ class loginViewSet(viewsets.ModelViewSet):
 class registerViewSet(viewsets.ModelViewSet):
     queryset = user.objects.all()
     serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        user = serializer.save()
+        Token.objects.create(user=user)
 
 class profileViewSet(viewsets.ModelViewSet):
     queryset = user.objects.all()
     serializer_class = UserSerializer
 
-class userViewSet(viewsets.ModelViewSet):
+class modelViewSet(viewsets.ModelViewSet):
     queryset = user.objects.all()
     serializer_class = UserSerializer
+
